e6064b983d456158064f9984ccb3bb5f8104b78f
Eduard Vlasov <59623754+QuickLike@users.noreply.github.com>
2024-06-06 00:37:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ import os
 from aiogram import Bot
 from dotenv import load_dotenv
 
+import constraints
 from currency import get_currency
 
-bot_work_type = 'updates'
-# 'updates' - присылать данные при обновлении на сайте
-# 'schedule' - по расписанию, время отправки берется из константы SEND_TIME
+bot_work_type = constraints.UPDATES
+# UPDATES - присылать данные при обновлении на сайте
+# SCHEDULE - по расписанию, время отправки берется из константы SEND_TIME
 
 SEND_TIME = 10
 # Время указывается в часах
@@ -18,31 +19,30 @@ SEND_TIME = 10
 
 async def main():
     load_dotenv()
-    token = os.getenv('BOT_TOKEN')
+    token = os.getenv('BOT_TOKEN', '')
+    user_id = os.getenv('USER_ID', '')
     bot = Bot(token)
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         filename='log.log',
     )
-    currency = get_currency()
+    try:
+        currency = get_currency()
+    except Exception as e:
+        logging.error(e)
+        return
     while True:
-        if bot_work_type == 'updates':
+        if bot_work_type == constraints.UPDATES:
             if currency != get_currency():
                 currency = get_currency()
-                await bot.send_message(343798461, text=f'Покупка: {currency[0]}\nПродажа: {currency[1]}')
-        elif bot_work_type == 'schedule':
+                await bot.send_message(user_id, text=f'Покупка: {currency[0]}\nПродажа: {currency[1]}')
+        elif bot_work_type == constraints.SCHEDULE:
             if not isinstance(SEND_TIME, int):
                 raise ValueError(f'Переменная SEND_TIME должна быть целочисленной.\n{SEND_TIME=}')
             if datetime.today().hour == SEND_TIME:
-                try:
-                    currency = get_currency()
-                except Exception as e:
-                    logging.error(e)
-                    await asyncio.sleep(10)
-                else:
-                    await bot.send_message(343798461, text=f'Покупка: {currency[0]}\nПродажа: {currency[1]}')
-                    await asyncio.sleep(3600)
+                await bot.send_message(user_id, text=f'Покупка: {currency[0]}\nПродажа: {currency[1]}')
+                await asyncio.sleep(3600)
         else:
             raise TypeError(f'Неизвестный тип работы бота! {bot_work_type=}\nДопустимые типы: updates, schedule')
 
