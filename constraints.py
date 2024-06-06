3ac692fa26ed424b7049ea49ffdd90d0590e2334
quicklike <quicklikerfree@gmail.com>
2024-06-06 18:36:15 +0300

    Настроил логгирование, исправил ошибки

--- constraints.py
+++ constraints.py
@@ -1,2 +1,4 @@
 UPDATES = 'updates'
 SCHEDULE = 'schedule'
+
+SEND_MESSAGE = 'Покупка: {currency[0]}\nПродажа: {currency[1]}'
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 from datetime import datetime
 import logging
 import os
+import sys
 
 from aiogram import Bot
 from dotenv import load_dotenv
@@ -22,11 +23,6 @@ async def main():
     token = os.getenv('BOT_TOKEN', '')
     user_id = os.getenv('USER_ID', '')
     bot = Bot(token)
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        filename='log.log',
-    )
     try:
         currency = get_currency()
     except Exception as e:
@@ -34,19 +30,40 @@ async def main():
         return
     while True:
         if bot_work_type == constraints.UPDATES:
+            await asyncio.sleep(10)
             if currency != get_currency():
                 currency = get_currency()
-                await bot.send_message(user_id, text=f'Покупка: {currency[0]}\nПродажа: {currency[1]}')
+                logging.info(constraints.SEND_MESSAGE.format(currency=currency))
+                await bot.send_message(
+                    chat_id=user_id,
+                    text=constraints.SEND_MESSAGE.format(currency)
+                )
         elif bot_work_type == constraints.SCHEDULE:
             if not isinstance(SEND_TIME, int):
                 raise ValueError(f'Переменная SEND_TIME должна быть целочисленной.\n{SEND_TIME=}')
             if datetime.today().hour == SEND_TIME:
-                await bot.send_message(user_id, text=f'Покупка: {currency[0]}\nПродажа: {currency[1]}')
+                logging.info(constraints.SEND_MESSAGE.format(currency=currency))
+                await bot.send_message(
+                    chat_id=user_id,
+                    text=constraints.SEND_MESSAGE.format(currency=currency)
+                )
                 await asyncio.sleep(3600)
         else:
             raise TypeError(f'Неизвестный тип работы бота! {bot_work_type=}\nДопустимые типы: updates, schedule')
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.INFO,
+        format=('%(asctime)s, '
+                '%(levelname)s, '
+                '%(funcName)s, '
+                '%(lineno)d, '
+                '%(message)s'
+                ),
+        encoding='UTF-8',
+        handlers=[logging.FileHandler(__file__ + '.log'),
+                  logging.StreamHandler(sys.stdout)]
+    )
     logging.info('Бот запущен')
     asyncio.run(main())
